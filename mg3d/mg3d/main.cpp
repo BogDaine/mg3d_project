f2045ad0f32bdf1575398fa3b7d0d704aa10a7b2
bogdan_osian <osianbogdanradu@gmail.com>
2021-11-28 23:48:53 +0300

    The program can now open a glfw window

--- mg3d/mg3d/main.cpp
+++ mg3d/mg3d/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <glew.h>
+#include <glfw3.h>
+
+#include "glfwstuff.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+
+	
+	GLFWwindow* window;
+	InitWindow(window, "s");
+	while (1);
+
     return 0;
 }
